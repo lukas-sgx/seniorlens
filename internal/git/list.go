480a3b78beff88ecd574266ae6095574ac380192
lukas-sgx <lukas.soigneux@epitech.eu>
2026-04-10 23:46:29 +0200

    style: breakline change

--- internal/git/list.go
+++ internal/git/list.go
@@ -17,6 +17,7 @@ type Commit struct {
 
 func setHead(dir string) (*git.Repository, *plumbing.Reference, error) {
 	var head *plumbing.Reference
+
 	repo, err := git.PlainOpen(dir)
 	if err != nil {
 		return nil, nil, err
@@ -47,8 +48,8 @@ func appendCommit(commits *[]Commit, objects object.CommitIter) error {
 func listCommit(dir string) ([]Commit, error) {
 	var commits []Commit
 	var objects object.CommitIter
-	repo, head, err := setHead(dir)
 
+	repo, head, err := setHead(dir)
 	if err != nil {
 		return commits, err
 	}
